2a1c9264ba6c996cc3e407ead76198c0cb76a7cd
Kogan <koganp42@gmail.com>
2019-11-24 22:07:21 -0600

    Paused work on forecast. Local storage ~working, new buttons ~gen'ing
    ~ denotes it's not fully finished.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
     let appID = "94bd8525986db57aa34cbcb264fb52c9";
 
-    $(".query_btn").click(function(){
 
+
+    $(".query_btn").click(function(){
+        event.preventDefault();
         var query_param = $(this).prev().val();
 
         if ($(this).prev().attr("placeholder") == "City") {
@@ -20,42 +22,55 @@ $( document ).ready(function() {
         });
         $.getJSON(forecast,function(json){
             console.log(json);
-            for (i = 0; i < json.list.length; i + 8) {
+            //let jsonArray = json.list;
+            //if(i = 0; i < jsonArray.length; i + 8) {
                 //if statement and indexOf are supposed to be key here according to stephen
                 //Maybe instead try creating the cards as I loop the index
-                // $(".weather_image").attr("src", "http://openweathermap.org/img/w/" + json.list[i].weather[0].icon + ".png");
-                // $(".temperature").html(json.list[i].main.temp + "&#176F");
-                // $(".humidity").html(json.list[i].main.humidity + "%");
-                let cardTemplate = 
-                `<div class="card">
-                    <div class="card-body bg-primary text-white">
-                        <h5 class="card-title forecastDate">${json.list[i].main.date}</h5>
-                        <img class="weather_image" src="http://openweathermap.org/img/w/${json.list[i].weather[0].icon}.png" alt="Current Weather Image">
-                        <p class="temperature">${json.list[i].main.temp}&#176F</p>
-                        <p class="humidity">${json.list[i].main.humidity}%</p>
-                    </div>
-                </div>`
-                $("#forecastDiv").append(cardTemplate);
-            }
+                // $(".weather_image").attr("src", "http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png");
+                // $(".temperature").html(json.list[0].main.temp + "&#176F");
+                // $(".humidity").html(json.list[0].main.humidity + "%");
+
+                //Here I'm trying to use template literals to create a card for each data point I want and append those cards to my forecastDiv.
+            //     let cardTemplate = 
+            //     `<div class="card">
+            //         <div class="card-body bg-primary text-white">
+            //             <h5 class="card-title forecastDate">${json.list[i].main.date}</h5>
+            //             <img class="weather_image" src="http://openweathermap.org/img/w/${json.list[i].weather[0].icon}.png" alt="Current Weather Image">
+            //             <p class="temperature">${json.list[i].main.temp}&#176F</p>
+            //             <p class="humidity">${json.list[i].main.humidity}%</p>
+            //         </div>
+            //     </div>`
+            //     $("#forecastDiv").append(cardTemplate);
+            // }
         });
+        //Here I need to make it so every time the search button is clicked, it will create a li, with the city searched saved as the text of the li. The li should also be a button that, once clicked, searches that city again. These should also be saved to local storage.
+        //I'm creating an object that houses the search term that was just used and setting that to an array which is saved to local storage.
+        
+        let citySearchObject = {
+            city: query_param
+        };
+        let searchHistoryArray = JSON.parse(localStorage.getItem("searchHistroyArray"));
+        // if(searchHistoryArray.includes(citySearchObject.query_param) === true){
+        //     return;
+        // } else 
+        if (searchHistoryArray === null){
+            searchHistoryArray = [];
+            searchHistoryArray.push(citySearchObject);
+        } else {
+            searchHistoryArray.push(citySearchObject);
+        };
+        localStorage.setItem("searchHistoryArray", JSON.stringify(searchHistoryArray));
+        //Here I'll create the list of prior searches and make them clickable. 
+        let searchLi = $("<li>");
+        let liButton = $("<button>");
+        liButton.addClass("btn btn-default btn-sm");
+        liButton.attr("type", "button");
+        liButton.text(query_param);
+        searchLi.html(liButton);
+        $("#searchHistoryList").prepend(searchLi);
     })
 
-    // Optional Code for temperature conversion
-    var fahrenheit = true;
 
-    $("#convertToCelsius").click(function() {
-        if (fahrenheit) {
-            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
-        }
-        fahrenheit = false;
-    });
 
-    $("#convertToFahrenheit").click(function() {
-        if (fahrenheit == false) {
-            $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
-        }
-        fahrenheit = true;
-
-    });
-    
+    //Note to tomorrow-self: Next up is getting the search history list to populate as soon as the page is loaded.
 });
